e26ffe575dda366e632c0d4bef3f50909a2f87a7
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2024-12-26 21:42:22 +0200

    feat: add user endpoints

---
+++ app/src/users/users.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.createUser(createUserDto);
+  }
+
+  @Get()
+  getAllUsers() {
+    return this.usersService.getAllUsers();
+  }
+
+  @Get(':id')
+  getUserById(@Param('id') id: string) {
+    return this.usersService.getUserById(+id);
+  }
+
+  @Patch(':id')
+  updateUserById(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.updateUserById(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  deleteUserById(@Param('id') id: string) {
+    return this.usersService.deleteUserById(+id);
+  }
+}
