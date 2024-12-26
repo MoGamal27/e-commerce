b4927a3a0c7f327472556bf04719de4292e9fc05
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2024-12-26 21:39:55 +0200

    migrations db

---
+++ app/prisma/migrations/20241226180432_init_db/migration.sql
@@ -0,0 +1,27 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'USER');
+
+-- CreateEnum
+CREATE TYPE "Gender" AS ENUM ('MALE', 'FEMALE');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'USER',
+    "avatar" TEXT NOT NULL,
+    "age" INTEGER NOT NULL,
+    "phoneNumber" INTEGER NOT NULL,
+    "Address" TEXT NOT NULL,
+    "gender" "Gender" NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_phoneNumber_key" ON "User"("phoneNumber");
