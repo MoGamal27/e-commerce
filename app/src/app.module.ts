f6873e380d4529d04847c7915fba07550ed9a3cf
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2024-12-27 17:07:03 +0200

    add config env & JWT

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot(),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '600000000s' },
+    }),
+    UsersModule],
 })
 export class AppModule {}
