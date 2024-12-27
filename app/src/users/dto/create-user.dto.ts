ae92adac1906d0c2c8c71dfe96518ac0477bc28a
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2024-12-27 17:08:31 +0200

    fix issue role & gender from string to enum

--- app/src/users/dto/create-user.dto.ts
+++ app/src/users/dto/create-user.dto.ts
@@ -1,5 +1,5 @@
 import { IsString, IsNotEmpty, IsEmail, MaxLength, MinLength, IsEnum, IsOptional, IsUrl, IsNumber, IsPhoneNumber, Length, IsBoolean } from 'class-validator';
-
+import { Role, Gender } from '@prisma/client';
 export class CreateUserDto {
   @IsString({ message: 'Name must be a string' })
   @IsNotEmpty({ message: 'Name is required' })
@@ -17,13 +17,13 @@ export class CreateUserDto {
   @MaxLength(30, {message: 'Password must be at most 30 characters long'})
   password: string;
 
-  @IsEnum(['ADMIN', 'USER'], { message: 'Role must be ADMIN or USER' })
+  @IsEnum(Role, { message: 'Role must be ADMIN or USER' })
   @IsOptional()
-  role: string;
+  role: Role;
 
+  @IsOptional()
   @IsString({ message: 'Avatar must be a string' })
   @IsUrl({}, { message: 'Avatar must be a valid URL' })
-  @IsOptional()
   avatar: string;
 
   @IsNumber({}, { message: 'Age must be a number' })
@@ -40,9 +40,9 @@ export class CreateUserDto {
   @IsOptional()
   address: string;
 
-  @IsEnum(['MALE', 'FEMALE'], { message: 'Gender must be MALE or FEMALE' })
+  @IsEnum(Gender, { message: 'Gender must be MALE or FEMALE' })
   @IsOptional()
-  gender: string;
+  gender: Gender;
 
   @IsBoolean({ message: 'Is Active must be a boolean' })
   @IsEnum(['true', 'false'], { message: 'Is Active must be true or false' })
@@ -52,6 +52,6 @@ export class CreateUserDto {
   @IsString({ message: 'verification code must be a string' })
   @Length(6, 6, { message: 'verification code must be 6 digits' })
   @IsOptional()
-  verrificationCode: string;
+  verificationCode: string;
 
 }
