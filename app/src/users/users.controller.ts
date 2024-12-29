cc2ff11e9a6a678bcd3934bf8d4e51624f3b6e25
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2024-12-29 14:50:38 +0200

    add query to get all users

--- app/src/users/users.controller.ts
+++ app/src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ValidationPipe, UsePipes, UseGuards} from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ValidationPipe, UsePipes, UseGuards, Query} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -20,7 +20,7 @@ export class UsersController {
   }
 
   @Get()
-  getAllUsers() {
+  getAllUsers(@Query() query: any) {
     return this.usersService.getAllUsers();
   }
 
