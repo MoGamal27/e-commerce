44dc83c95b471d6d53898d7b4a74889efba4e608
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2024-12-31 14:54:04 +0200

    Update users.module.ts

--- app/src/users/users.module.ts
+++ app/src/users/users.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './services/Admins/users.service';
-import { UsersController } from './users.controller';
+import { singleUserService } from './services/User/singleUser.service';
+import { UsersController } from './controller/Admins/users.controller';
+import { singleUserController } from './controller/User/singleUser.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [UsersController],
-  providers: [UsersService],
+  controllers: [
+    UsersController, 
+    singleUserController
+  ],
+  providers: [
+    UsersService, 
+    singleUserService
+  ],
 })
 export class UsersModule {}
