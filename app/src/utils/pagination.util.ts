e154a43732c1e79c456fcf894c8942ee6ef87125
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2025-01-25 16:03:06 +0200

    fix issue order by to make it accept array of object

--- app/src/utils/pagination.util.ts
+++ app/src/utils/pagination.util.ts
@@ -5,12 +5,12 @@ export async function paginate<T>(
   prisma: PrismaService,
   model: string,
   query: any,
-  searchFields: string[] = ['name'],
+  searchFields: string[] = [],
   filterFields: Record<string, any> = {},
 ) {
   const { limit = 100, skip = 0, sort = 'asc', search, ...filters } = query;
 
- 
+  // Validate limit and skip
   if (Number.isNaN(Number(+limit))) {
     throw new HttpException('Invalid limit', 400);
   }
@@ -18,7 +18,7 @@ export async function paginate<T>(
     throw new HttpException('Invalid skip', 400);
   }
 
-  
+  // Validate sort
   if (!['asc', 'desc'].includes(sort)) {
     throw new HttpException('Sort must be asc or desc', 400);
   }
@@ -26,7 +26,7 @@ export async function paginate<T>(
   
   const where: any = {};
 
-  
+  //search functionality
   if (search) {
     where.OR = searchFields.map((field) => ({
       [field]: { contains: search, mode: 'insensitive' },
@@ -40,14 +40,17 @@ export async function paginate<T>(
     }
   }
 
+  //ORDER BY clause as an array of objects
+  const orderBy = searchFields.map((field) => ({
+    [field]: sort,
+  }));
+
   
   const data = await prisma[model].findMany({
     take: +limit,
     skip: +skip,
     where,
-    orderBy: {
-      [searchFields[0]]: sort,
-    },
+    orderBy, 
   });
 
   return {
