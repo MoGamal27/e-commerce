84882860ef0e28bd03ac0a6ae39d1356ba2e0eac
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2025-01-25 16:00:35 +0200

    add roles to endpoints

--- app/src/product/product.controller.ts
+++ app/src/product/product.controller.ts
@@ -1,13 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ValidationPipe } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ValidationPipe, Query, UseGuards } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Roles } from 'src/users/decorator/Roles.decorator';
+import { AuthGuard } from 'src/auth/guard/Auth.guard';
+
 
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
   @Post()
+  @Roles(['admin'])
+  @UseGuards(AuthGuard)
   create(@Body(new ValidationPipe({ forbidNonWhitelisted: true })) 
   createProductDto: CreateProductDto,
 ) {
@@ -15,8 +20,8 @@ export class ProductController {
   }
 
   @Get()
-  findAll() {
-    return this.productService.findAll();
+  findAll(@Query() query: any) {
+    return this.productService.findAll(query);
   }
 
   @Get(':id')
@@ -25,11 +30,15 @@ export class ProductController {
   }
 
   @Patch(':id')
+  @Roles(['admin'])
+  @UseGuards(AuthGuard)
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productService.update(+id, updateProductDto);
   }
 
   @Delete(':id')
+  @Roles(['admin'])
+  @UseGuards(AuthGuard)
   remove(@Param('id') id: string) {
     return this.productService.remove(+id);
   }
